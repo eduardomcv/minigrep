e8d8faea2c04bd188165639a003d211eb343bf58
Eduardo Vicente <eduardomcv42@gmail.com>
2023-01-08 02:42:15 +0000

    Add comment

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub struct Config {
 
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // Skip first value
         args.next();
 
         let (flags, other_args): (Vec<String>, Vec<String>) =
